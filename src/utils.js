cea61caffd3f45437ad88fd5359a650dd293c761
daniel <danielcohenn9@gmail.com>
2022-12-21 20:09:28 +0200

    first commit

--- src/utils.js
+++ src/utils.js
@@ -1,19 +1,19 @@
 import Axios from "axios";
 
-const allurl = 'http://ec2-54-209-155-37.compute-1.amazonaws.com:3001/api/'
+const allurl = 'http://ec2-54-209-155-37.compute-1.amazonaws.com:3001/api'
 
-const getAll = (url) => Axios.get(`${allurl}/${url}`);
+const getAll = (url) => Axios.get(`${allurl}${url}`);
 
 const getAllapi = (url) => Axios.get(url);
 
-const getById = (url, id) => Axios.get(`${allurl}/${url}/${id}`);
+const getById = (url, id) => Axios.get(`${allurl}${url}/${id}`);
 
-const addObj = (url, obj, key) => Axios.post(`${allurl}/${url}`, obj,{headers:{"x-api-key":key}});
+const addObj = (url, obj, key) => Axios.post(`${allurl}${url}`, obj,{headers:{"x-api-key":key}});
 
-const updateObj = (url, id, obj, key) => Axios.put(`${allurl}/${url}/${id}`, obj,{headers:{"x-api-key":key}});
+const updateObj = (url, id, obj, key) => Axios.put(`${allurl}${url}/${id}`, obj,{headers:{"x-api-key":key}});
 
-const deleteObj = (url, id, key) => Axios.delete(`${allurl}/${url}/${id}`,{headers:{"x-api-key":key}});
+const deleteObj = (url, id, key) => Axios.delete(`${allurl}${url}/${id}`,{headers:{"x-api-key":key}});
 
-const gettodosbyid = (url, id) => Axios.get(`${allurl}/${url}?userId=${id}`);
+const gettodosbyid = (url, id) => Axios.get(`${allurl}${url}?userId=${id}`);
 
 export { getAll, getById, addObj, updateObj, deleteObj, gettodosbyid,getAllapi };
